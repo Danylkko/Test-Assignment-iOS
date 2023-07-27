233b5a53e8f6cce7f08d0e272be7987a4f4dbcd5
Danylo Litvinchuk <ltvnchkd@gmail.com>
2023-07-27 18:33:40 +0300

    coordinator flow was set up

---
+++ Test-Assignment-iOS/Test-Assignment-iOS/MainScreen/MainCoordinator.swift
@@ -0,0 +1,30 @@
+//
+//  MainCoordinator.swift
+//  Test-Assignment-iOS
+//
+//  Created by Danylo Litvinchuk on 27.07.2023.
+//
+
+import UIKit
+
+class MainCoordinator: NSObject, Coordinator, UINavigationControllerDelegate {
+    
+    func childDidFinish(_ child: Coordinator?) {
+        
+    }
+    
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func start() {
+        navigationController.delegate = self
+        let vc = MainViewController.instantiate()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: false)
+    }
+    
+}
---
+++ Test-Assignment-iOS/Test-Assignment-iOS/Utilites/Storyboarded.swift
@@ -0,0 +1,20 @@
+//
+//  Storyboarded.swift
+//  Test-Assignment-iOS
+//
+//  Created by Danylo Litvinchuk on 27.07.2023.
+//
+
+import UIKit
+
+protocol Storyboarded {
+    static func instantiate() -> Self
+}
+
+extension Storyboarded where Self: UIViewController {
+    static func instantiate() -> Self {
+        let id = String(describing: self)
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        return storyboard.instantiateViewController(withIdentifier: id) as! Self
+    }
+}
