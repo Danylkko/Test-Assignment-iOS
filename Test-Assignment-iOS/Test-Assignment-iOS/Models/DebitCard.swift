2263aae768144a586ef792ececb5991303c5ebd0
Danylo Litvinchuk <ltvnchkd@gmail.com>
2023-07-27 20:38:51 +0300

    DebitCard brand property added

--- Test-Assignment-iOS/Test-Assignment-iOS/Models/DebitCard.swift
+++ Test-Assignment-iOS/Test-Assignment-iOS/Models/DebitCard.swift
@@ -10,4 +10,20 @@ import Foundation
 struct DebitCard: Card {
     var number: String
     var date: Date
+    
+    var brand: Brand? {
+        switch number.first {
+        case "4":
+            return .visa
+        case "5":
+            return .mastercard
+        default:
+            return nil
+        }
+    }
+    
+    enum Brand {
+        case visa
+        case mastercard
+    }
 }
