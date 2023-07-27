0202e28f9204a7f03d732f9eab3cc09e62040bac
Danylo Litvinchuk <ltvnchkd@gmail.com>
2023-07-27 20:07:33 +0300

    JSONCoder defined

---
+++ Test-Assignment-iOS/Test-Assignment-iOS/Models/JSONCoder.swift
@@ -0,0 +1,31 @@
+//
+//  JSONCoder.swift
+//  Test-Assignment-iOS
+//
+//  Created by Danylo Litvinchuk on 27.07.2023.
+//
+
+import Foundation
+
+class JSONCoder {
+    
+    static func encode<T: Codable>(_ object: T) throws -> String {
+        let encoder = JSONEncoder()
+        let data = try encoder.encode(object)
+        guard let jsonString = String(data: data, encoding: .utf8) else {
+            // TODO: - make prettier
+            fatalError()
+        }
+        return jsonString
+    }
+    
+    static func decode<T: Codable>(_ jsonString: String) throws -> T {
+        let decoder = JSONDecoder()
+        guard let data = jsonString.data(using: .utf8) else {
+            // TODO: - make prettier
+            fatalError()
+        }
+        let object = try decoder.decode(T.self, from: data)
+        return object
+    }
+}
